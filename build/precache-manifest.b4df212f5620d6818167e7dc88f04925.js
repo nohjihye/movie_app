edc539763cf9d9bb4fe67a0b61c62aec1b43d95f
nohjihye <nohjihye@cj.net>
2020-03-20 18:11:00 +0900

    build

---
+++ build/precache-manifest.b4df212f5620d6818167e7dc88f04925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324ef189488a4ec85e28611e1f0e71a8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f4f186a5ebdac555489e",
+    "url": "/movie_app/static/css/main.af6e779a.chunk.css"
+  },
+  {
+    "revision": "1b32295779426c291dc4",
+    "url": "/movie_app/static/js/2.525870d7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie_app/static/js/2.525870d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f186a5ebdac555489e",
+    "url": "/movie_app/static/js/main.b158879b.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
