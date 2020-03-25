a72ae382c643b75b0a3c2fe5eab0d4f440c2eb70
nohjihye <nohjihye@cj.net>
2020-03-25 17:10:28 +0900

    styled component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Movie.css";
+import styled from "styled-components";
 
+const MovieBox = styled.div`
+    width: 45%; background-color: white; margin-bottom: 70px;font-weight: 300;padding: 20px; border-radius: 5px; color: #adaeb9;box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25), 0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+    a {
+      display: flex;align-items: flex-start;justify-content: space-between;text-decoration: none;color: inherit;
+    }
+    img {
+      position: relative;top: -50px; max-width: 150px;width: 100%;margin-right: 30px; box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+    }
+    h3 {
+        margin: 0;font-weight: 300; margin-bottom: 5px;font-size: 24px; color: #2c2c2c;
+    }
+    h5 {
+        margin-right: 10px;font-size: 14px;font-weight: 300;
+    }
+    ul {
+        list-style: none;padding: 0; margin: 0; display: flex;margin: 5px 0px;
+        li { margin-right: 10px; font-size: 14px;}
+    }
+`
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
+    <MovieBox>
       <Link
         to={{
           pathname: `/movie/${id}`,
@@ -19,20 +38,20 @@ function Movie({ id, year, title, summary, poster, genres }) {
         }}
       >
         <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
-          <ul className="movie__genres">
+        <div>
+          <h3>{title}</h3>
+          <h5>{year}</h5>
+          <ul>
             {genres.map((genre, index) => (
               <li key={index} className="genres__genre">
                 {genre}
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+          <p>{summary.slice(0, 180)}...</p>
         </div>
       </Link>
-    </div>
+    </MovieBox>
   );
 }
 
